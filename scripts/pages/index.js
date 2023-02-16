3d0331628d643a05ed29ebcdfacfc6596077242a
hugowoille <hugo.woille@gmail.com>
2023-02-16 11:12:03 +0100

    push

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,7 +1,7 @@
 async function getPhotographers() {
     // Ceci est un exemple de données pour avoir un affichage de photographes de test dès le démarrage du projet, 
     // mais il sera à remplacer avec une requête sur le fichier JSON en utilisant "fetch".
-    let mesphotographes = [
+    /* let mesphotographes = [
         {
             "name": "Ma data test",
             "id": 1,
@@ -20,16 +20,25 @@ async function getPhotographers() {
             "price": 500,
             "portrait": "account.png"
         },
-    ];
-    // et bien retourner le tableau photographers seulement une fois récupéré
-    return ({
-        photographers: [...mesphotographes, ...mesphotographes, ...mesphotographes]
-    });
+    ]; */
+    // et bien retourner le tableau photographers seulement une fois récupéré https://hugowoille.github.io/Front-End-Fisheye/data/photographers.json
+    /*  return ({
+         photographers: [mesphotographes, ...mesphotographes, ...mesphotographes]
+     }); */
+    try {
+        const apiReponse = await fetch("./data/photographers.json");
+        const data = await apiReponse.json();
+        return data;
+    } catch (error) {
+        console.log(error);
+    }
+
 }
 async function displayData(photographers) {
     const photographersSection = document.querySelector(".photographer_section");
 
     photographers.forEach((photographer) => {
+        // eslint-disable-next-line no-undef
         const photographerModel = photographerFactory(photographer);
         const userCardDOM = photographerModel.getUserCardDOM();
         photographersSection.appendChild(userCardDOM);
@@ -40,7 +49,6 @@ async function init() {
     // Récupère les datas des photographes
     const { photographers } = await getPhotographers();
     displayData(photographers);
-    console.log({ toto: "hello", ...photographers[0] });
 }
 
 init();
